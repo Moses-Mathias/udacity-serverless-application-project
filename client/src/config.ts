910c848a53642d523800dea70d9300a4b98b41c1
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-07-26 15:26:41 +0300

    ToDo application on AWS serverless platform

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '8yhcle6zn5';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-9ewchwt1.us.auth0.com',
+  clientId: 'u0DXI0QFKZy70v0Av4l16I2dKvSRC5EZ',
+  callbackUrl: 'http://localhost:3000/callback'
+};
